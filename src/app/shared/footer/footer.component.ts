949ed7d1f4a129eb732713844f8a01f4269053df
Daniel Jancar <danieljancar@bluewin.ch>
2024-06-09 21:03:39 +0200

    feat: initialize footer component

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar'
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [
+    MatToolbar,
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
