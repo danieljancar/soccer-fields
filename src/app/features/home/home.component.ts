42eddc6ea85fe05678c34c3ce1e7ffbfa099ff4a
Daniel Jancar <danieljancar@bluewin.ch>
2024-06-14 15:10:09 +0200

    feat: add readme to homescreen with ngx-markdown

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { MarkdownComponent } from 'ngx-markdown'
+import { HttpClient } from '@angular/common/http'
+import { Subscription } from 'rxjs'
+import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card'
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [],
+    imports: [MarkdownComponent, MatCard, MatCardContent, MatCardHeader],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit, OnDestroy {
+    constructor(private http: HttpClient) {}
+
+    private subscription: Subscription = new Subscription()
+    protected readme: string | undefined
+
+    ngOnInit() {
+        this.subscription.add(
+            this.http
+                .get(
+                    'https://raw.githubusercontent.com/danieljancar/soccer-fields/master/README.md',
+                    { responseType: 'text' }
+                )
+                .subscribe((data) => {
+                    this.readme = data
+                })
+        )
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe()
+    }
+}
