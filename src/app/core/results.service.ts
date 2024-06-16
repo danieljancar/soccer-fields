9975c934788aa2742f8c7df74c9619a56503f74e
Daniel Jancar <danieljancar@bluewin.ch>
2024-06-16 17:48:37 +0200

    feat: add proper league selection handling

--- src/app/core/results.service.ts
+++ src/app/core/results.service.ts
@@ -6,97 +6,92 @@ import { DataTableItem } from '../features/data-table/data-table-datasource'
 import { HttpClient } from '@angular/common/http'
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class ResultsService {
-    private jsonUrl = '../../../results.json'
-    private leaderboardSubject = new BehaviorSubject<DataTableItem[]>([])
-    leaderboard$ = this.leaderboardSubject.asObservable()
+  private jsonUrl = '../../../results.json'
+  private leaderboardSubject = new BehaviorSubject<DataTableItem[]>([])
+  leaderboard$ = this.leaderboardSubject.asObservable()
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-    getResults(): Observable<Results> {
-        return this.http.get<Results>(this.jsonUrl)
-    }
+  getResults(): Observable<Results> {
+    return this.http.get<Results>(this.jsonUrl)
+  }
 
-    setFilter(league: string, maxDay: number): void {
-        this.getResults()
-            .pipe(
-                map((results) => {
-                    const leaderboard: Record<string, DataTableItem> = {}
+  getAvailableLeagues(): Observable<string[]> {
+    return this.getResults().pipe(
+      map((results) => Object.keys(results)),
+    )
+  }
 
-                    const leagueData = results[league as keyof Results]
+  setFilter(league: string, maxDay: number): void {
+    this.getResults().pipe(
+      map((results) => {
+        const leaderboard: Record<string, DataTableItem> = {}
+        const leagueData = results[league as keyof Results]
 
-                    for (const day in leagueData) {
-                        if (parseInt(day.replace('day', ''), 10) > maxDay)
-                            continue
+        for (const day in leagueData) {
+          if (parseInt(day.replace('day', ''), 10) > maxDay) continue
 
-                        leagueData[day].forEach((match) => {
-                            this.updateTeamStats(
-                                leaderboard,
-                                match.home,
-                                match.away
-                            )
-                            this.updateTeamStats(
-                                leaderboard,
-                                match.away,
-                                match.home
-                            )
-                        })
-                    }
-
-                    const sortedLeaderboard = Object.values(leaderboard).sort(
-                        (a, b) =>
-                            b.points - a.points ||
-                            b.goalDiff - a.goalDiff ||
-                            b.wins - a.wins ||
-                            a.name.localeCompare(b.name)
-                    )
-
-                    sortedLeaderboard.forEach((team, index) => {
-                        team.rank = index + 1
-                    })
-
-                    this.leaderboardSubject.next(sortedLeaderboard)
-                })
-            )
-            .subscribe()
-    }
-
-    private updateTeamStats(
-        leaderboard: Record<string, DataTableItem>,
-        team: { name: string; score: number },
-        opponent: { name: string; score: number }
-    ) {
-        if (!leaderboard[team.name]) {
-            leaderboard[team.name] = {
-                name: team.name,
-                rank: 0,
-                points: 0,
-                matchesPlayed: 0,
-                wins: 0,
-                losses: 0,
-                draws: 0,
-                scoredGoals: 0,
-                receivedGoals: 0,
-                goalDiff: 0,
-            }
+          leagueData[day].forEach((match) => {
+            this.updateTeamStats(leaderboard, match.home, match.away)
+            this.updateTeamStats(leaderboard, match.away, match.home)
+          })
         }
 
-        const teamStats = leaderboard[team.name]
-        teamStats.scoredGoals += team.score
-        teamStats.receivedGoals += opponent.score
-        teamStats.goalDiff = teamStats.scoredGoals - teamStats.receivedGoals
-        teamStats.matchesPlayed += 1
+        const sortedLeaderboard = Object.values(leaderboard).sort(
+          (a, b) =>
+            b.points - a.points ||
+            b.goalDiff - a.goalDiff ||
+            b.wins - a.wins ||
+            a.name.localeCompare(b.name),
+        )
 
-        if (team.score > opponent.score) {
-            teamStats.wins += 1
-            teamStats.points += 3
-        } else if (team.score < opponent.score) {
-            teamStats.losses += 1
-        } else {
-            teamStats.draws += 1
-            teamStats.points += 1
-        }
+        sortedLeaderboard.forEach((team, index) => {
+          team.rank = index + 1
+        })
+
+        this.leaderboardSubject.next(sortedLeaderboard)
+      }),
+    ).subscribe()
+  }
+
+  private updateTeamStats(
+    leaderboard: Record<string, DataTableItem>,
+    team: { name: string; score: number },
+    opponent: { name: string; score: number },
+  ) {
+    if (!leaderboard[team.name]) {
+      leaderboard[team.name] = {
+        name: team.name,
+        rank: 0,
+        points: 0,
+        matchesPlayed: 0,
+        wins: 0,
+        losses: 0,
+        draws: 0,
+        scoredGoals: 0,
+        receivedGoals: 0,
+        goalDiff: 0,
+      }
     }
+
+    const teamStats = leaderboard[team.name]
+    teamStats.scoredGoals += team.score
+    teamStats.receivedGoals += opponent.score
+    teamStats.goalDiff = teamStats.scoredGoals - teamStats.receivedGoals
+    teamStats.matchesPlayed += 1
+
+    if (team.score > opponent.score) {
+      teamStats.wins += 1
+      teamStats.points += 3
+    } else if (team.score < opponent.score) {
+      teamStats.losses += 1
+    } else {
+      teamStats.draws += 1
+      teamStats.points += 1
+    }
+  }
 }
--- src/app/features/leaderboard/leaderboard.component.ts
+++ src/app/features/leaderboard/leaderboard.component.ts
@@ -1,27 +1,52 @@
-import { Component, inject } from '@angular/core'
-import { DataTableComponent } from '../data-table/data-table.component'
+import { Component, inject, OnInit } from '@angular/core'
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 import { ResultsService } from '../../core/results.service'
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { MatFormField, MatLabel } from '@angular/material/form-field'
+import { MatOption, MatSelect } from '@angular/material/select'
+import { DataTableComponent } from '../data-table/data-table.component'
+import { MatButton } from '@angular/material/button'
+import { MatInput } from '@angular/material/input'
 
 @Component({
-    selector: 'app-leaderboard',
-    standalone: true,
-    imports: [DataTableComponent, ReactiveFormsModule],
-    templateUrl: './leaderboard.component.html',
-    styleUrl: './leaderboard.component.scss',
+  selector: 'app-leaderboard',
+  templateUrl: './leaderboard.component.html',
+  styleUrls: ['./leaderboard.component.scss'],
+  standalone: true,
+  imports: [
+    MatOption,
+    MatSelect,
+    MatLabel,
+    MatFormField,
+    ReactiveFormsModule,
+    DataTableComponent,
+    MatButton,
+    MatInput,
+  ],
 })
-export class LeaderboardComponent {
-    protected resultsService = inject(ResultsService)
-    leagueControl = new FormControl('bundesliga')
-    dayControl = new FormControl(1)
-    form = new FormGroup({
-        league: this.leagueControl,
-        day: this.dayControl,
-    })
+export class LeaderboardComponent implements OnInit {
+  protected resultsService = inject(ResultsService)
+  leagues: string[] = []
+  leagueControl = new FormControl('', Validators.required)
+  dayControl = new FormControl(1, [Validators.required, Validators.min(1)])
+  form = new FormGroup({
+    league: this.leagueControl,
+    day: this.dayControl,
+  })
 
-    onSubmit(): void {
-        const league = this.form.value.league as string
-        const day = this.form.value.day as number
-        this.resultsService.setFilter(league, day)
+  ngOnInit(): void {
+    this.resultsService.getAvailableLeagues().subscribe((leagues) => {
+      this.leagues = leagues
+      if (this.leagues.length > 0) {
+        this.leagueControl.setValue(this.leagues[0])
+      }
+    })
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      const league = this.form.value.league as string
+      const day = this.form.value.day as number
+      this.resultsService.setFilter(league, day)
     }
+  }
 }
