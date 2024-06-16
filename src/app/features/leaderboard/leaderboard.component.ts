b64f11a82b9c7c92fa7bd78c2973cd6695abf9e4
danieljancar <danieljancar@users.noreply.github.com>
2024-06-16 15:52:01 +0000

    Prettified Code!

--- src/app/features/leaderboard/leaderboard.component.ts
+++ src/app/features/leaderboard/leaderboard.component.ts
@@ -1,5 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core'
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import {
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms'
 import { ResultsService } from '../../core/results.service'
 import { MatFormField, MatLabel } from '@angular/material/form-field'
 import { MatOption, MatSelect } from '@angular/material/select'
@@ -8,45 +13,45 @@ import { MatButton } from '@angular/material/button'
 import { MatInput } from '@angular/material/input'
 
 @Component({
-  selector: 'app-leaderboard',
-  templateUrl: './leaderboard.component.html',
-  styleUrls: ['./leaderboard.component.scss'],
-  standalone: true,
-  imports: [
-    MatOption,
-    MatSelect,
-    MatLabel,
-    MatFormField,
-    ReactiveFormsModule,
-    DataTableComponent,
-    MatButton,
-    MatInput,
-  ],
+    selector: 'app-leaderboard',
+    templateUrl: './leaderboard.component.html',
+    styleUrls: ['./leaderboard.component.scss'],
+    standalone: true,
+    imports: [
+        MatOption,
+        MatSelect,
+        MatLabel,
+        MatFormField,
+        ReactiveFormsModule,
+        DataTableComponent,
+        MatButton,
+        MatInput,
+    ],
 })
 export class LeaderboardComponent implements OnInit {
-  protected resultsService = inject(ResultsService)
-  leagues: string[] = []
-  leagueControl = new FormControl('', Validators.required)
-  dayControl = new FormControl(1, [Validators.required, Validators.min(1)])
-  form = new FormGroup({
-    league: this.leagueControl,
-    day: this.dayControl,
-  })
-
-  ngOnInit(): void {
-    this.resultsService.getAvailableLeagues().subscribe((leagues) => {
-      this.leagues = leagues
-      if (this.leagues.length > 0) {
-        this.leagueControl.setValue(this.leagues[0])
-      }
+    protected resultsService = inject(ResultsService)
+    leagues: string[] = []
+    leagueControl = new FormControl('', Validators.required)
+    dayControl = new FormControl(1, [Validators.required, Validators.min(1)])
+    form = new FormGroup({
+        league: this.leagueControl,
+        day: this.dayControl,
     })
-  }
 
-  onSubmit(): void {
-    if (this.form.valid) {
-      const league = this.form.value.league as string
-      const day = this.form.value.day as number
-      this.resultsService.setFilter(league, day)
+    ngOnInit(): void {
+        this.resultsService.getAvailableLeagues().subscribe((leagues) => {
+            this.leagues = leagues
+            if (this.leagues.length > 0) {
+                this.leagueControl.setValue(this.leagues[0])
+            }
+        })
     }
-  }
+
+    onSubmit(): void {
+        if (this.form.valid) {
+            const league = this.form.value.league as string
+            const day = this.form.value.day as number
+            this.resultsService.setFilter(league, day)
+        }
+    }
 }
